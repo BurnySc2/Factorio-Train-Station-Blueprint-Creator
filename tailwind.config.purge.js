638bb756251b5250fa2d39cc5614137bad8229ca
BuRny <gamingburny@gmail.com>
2021-01-13 11:00:09 +0100

    Create a purge and a dev tailwindcss config

--- tailwind.config.purge.js
+++ tailwind.config.purge.js
@@ -3,7 +3,10 @@ module.exports = {
         removeDeprecatedGapUtilities: true,
         purgeLayersByDefault: true,
     },
-    purge: {},
+    purge: {
+        enabled: true,
+        content: ["./src/**/*.{tsx,ts}"],
+    },
     theme: {
         extend: {},
     },
